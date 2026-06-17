060c2e63c2062514baa40eaa3fc034687564526b
hanh-cutephomaiques1vn <hanhnttd01294@gmail.com>
2026-06-17 16:27:11 +0700

    Week 5 ASP.NET Core WebAPI Basics

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models;
+
+namespace ProductApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private static List<Product> products =
+    [
+        new Product
+        {
+            Id = 1,
+            Name = "Laptop",
+            Price = 15000000
+        },
+
+        new Product
+        {
+            Id = 2,
+            Name = "Mouse",
+            Price = 200000
+        }
+    ];
+
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProduct(int id)
+    {
+        var product = products.FirstOrDefault(x => x.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+    [HttpPost]
+    public IActionResult PostProduct(Product product)
+    {
+        products.Add(product);
+
+        return CreatedAtAction(
+            nameof(GetProduct),
+            new { id = product.Id },
+            product);
+    }
+}
+    
+
+
